ddb5d4cdbec888118cdea9b7123a5361e06ba48b
doit132 <wouitc132@qq.com>
2024-03-12 13:18:49 +0800

    🦄 refactor:

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
 #include "i2c.h"
 #include "tim.h"
 #include "usart.h"
@@ -26,6 +27,21 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "test.h"
+
+/* 板级支持包相关 */
+#include "led.h"
+#include "oled.h"
+#include "mpu6050.h"
+#include "hcsr04.h"
+#include "encoder.h"
+#include "motor.h"
+#include "key.h"
+#include "show.h"
+
+/* 辅助文件 */
+#include "delay.h"
+#include "soft_timer.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,10 +62,24 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-float pAccelOut[3];
-float pTempOut;
-float pGyroOut[3];
-float pEularAngleOut[3];
+Encoder_Data_t Encoder_Data;
+Motor_Data_t   Motor_Data;
+MPU6050_Data_t MPU6050_Data;
+Hcsr04Info_t   Hcsr04Info;
+
+u8          Way_Angle = 2; /* 获取角度的算法, 1: 四元数  2: 卡尔曼  3: 互补滤波 */
+u8          Flag_front, Flag_back, Flag_Left, Flag_Right, Flag_velocity = 2; /* 蓝牙遥控相关的变量 */
+u8          Flag_Stop = 1, Flag_Show = 0; /* 电机停止标志位和显示标志位  默认停止 显示打开 */
+int         Voltage;                      /* 电池电压采样相关的变量 */
+float       Angle_Balance, Gyro_Balance, Gyro_Turn; /* 平衡倾角 平衡陀螺仪 转向陀螺仪 */
+u8          PID_Send;                               /* 调参相关变量 */
+u8          Flag_follow = 0, Flag_avoid = 0;        /* 超声波跟随、超声波壁障标志位 */
+volatile u8 delay_flag, delay_50;                   /* 提供延时的变量 */
+float       Balance_Kp = 22500, Balance_Kd = 108;   /* 定义平衡环 PID 参数 */
+float       Velocity_Kp = 16000, Velocity_Ki = 80;  /* 定义速度环 PID 参数 */
+float       Turn_Kp = 4200, Turn_Kd = 0;            /* 定义转向环 PID 参数 PID 参数 (放大 100 倍) */
+int         Motor_Left, Motor_Right;                /* 存放电机经过 PID 控制之后的 PWM */
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -60,7 +90,16 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+static void BSP_Init(void)
+{
+    OLED_Init();
+    Encoder_Init(&Encoder_Data);
+    Motor_Init(&Motor_Data);
+    MPU6050_Init(&MPU6050_Data);
+    HCSR04_Init(&htim3, TIM_CHANNEL_3);
+    OLED_Init();
+    SoftTimer_Init();
+}
 /* USER CODE END 0 */
 
 /**
@@ -97,7 +136,12 @@ int main(void)
     MX_TIM2_Init();
     MX_TIM1_Init();
     MX_TIM4_Init();
+    MX_ADC1_Init();
     /* USER CODE BEGIN 2 */
+    BSP_Init();
+    /* 原本由 CubeMX 工具生成的代码是 HAL_NVIC_EnableIRQ(EXTI15_10_IRQn) 由于生成的代码总在用户代码之上
+    这个中断是需要所有的外设初始化完毕最后才能开启的, 所以在 MX_GPIO_Init 中先失能, 在所有外设初始化完成后再使能 */
+    // HAL_NVIC_EnableIRQ(EXTI15_10_IRQn); /* 开启引脚外部中断 */
 
     /* USER CODE END 2 */
 
@@ -112,7 +156,18 @@ int main(void)
         // Test_MPU6050();
         // Test_OLED();
         // Test_Main();
-        Test_SoftTimer();
+        // Test_ADC();
+        // Test_SoftTimer();
+
+        if (Flag_Show == 0) /* 使用 OLED 显示屏 */
+        {
+            OLED_Show();
+        }
+        delay_flag = 1;
+        delay_50 = 0;
+        while (delay_flag)
+            ; /* 示波器需要 50ms 高精度延时, delay 函数不满足要求, 故使用 MPU6050 中断提供 50ms 延时 */
+
         /* USER CODE END WHILE */
 
         /* USER CODE BEGIN 3 */
@@ -126,8 +181,9 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef       RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef       RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
     /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -153,6 +209,12 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+    PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
     {
         Error_Handler();
     }
--- System/sys/sys.h
+++ System/sys/sys.h
@@ -7,13 +7,33 @@ extern "C" {
 
 /* ANCHOR - 头文件包含 */
 
-// #include "main.h"
+#include "main.h"
+#include "encoder.h"
+#include "motor.h"
+#include "hcsr04.h"
+#include "mpu6050.h"
+
+/* ANCHOR - 全局变量 */
+
+extern u8          Way_Angle; /* 获取角度的算法, 1: 四元数  2: 卡尔曼  3: 互补滤波 */
+extern u8          Flag_front, Flag_back, Flag_Left, Flag_Right, Flag_velocity; /* 蓝牙遥控相关的变量 */
+extern u8          Flag_Stop, Flag_Show; /* 电机停止标志位和显示标志位  默认停止 显示打开 */
+extern int         Voltage;              /* 电池电压采样相关的变量 */
+extern float       Angle_Balance, Gyro_Balance, Gyro_Turn; /* 平衡倾角 平衡陀螺仪 转向陀螺仪 */
+extern u8          PID_Send;                               /* 调参相关变量 */
+extern u8          Flag_follow, Flag_avoid;                /* 超声波跟随、超声波壁障标志位 */
+extern volatile u8 delay_flag, delay_50;                   /* 提供延时的变量 */
+extern float       Balance_Kp, Balance_Kd;
+extern float       Velocity_Kp, Velocity_Ki;
+extern float       Turn_Kp, Turn_Kd;        /* PID 参数(放大 100 倍) */
+extern int         Motor_Left, Motor_Right; /* 存放电机经过 PID 控制之后的 PWM */
+
+extern Encoder_Data_t Encoder_Data;
+extern Motor_Data_t   Motor_Data;
+extern MPU6050_Data_t MPU6050_Data;
+extern Hcsr04Info_t   Hcsr04Info;
 
 /* ANCHOR - typedef */
-
-typedef unsigned int   u32; // u32
-typedef unsigned char  u8;  // u8
-typedef unsigned short u16; // u16
 
 /* ANCHOR - 公共函数声明 */
 
